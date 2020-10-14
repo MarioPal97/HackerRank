5adf14fa10333db1757ffc3129dec1f55a4607c2
Mario Palacios <48101008+MarioPal97@users.noreply.github.com>
2020-10-14 13:59:06 -0700

    Create Solution.java

---
+++ EasyCodes/miniMaxSum/Solution.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        long max = 0; long min = arr[0]; long sum = 0;
+        for (int i = 0; i < 5; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            } else if (arr[i] > max) {
+                max = arr[i];
+            }
+            sum = sum + arr[i];
+        }
+        long sumMax = sum - min;
+        long sumMin = sum - max; 
+        System.out.print(sumMin + " " + sumMax);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
